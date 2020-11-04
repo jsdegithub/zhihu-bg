a6b0c6a7595b8436f2ff8806c9e82355adaaf120
jsdegithub <def_myself@qq.com>
2020-11-05 05:49:50 +0800

    给各模型添加timestamps时间戳

--- app/models/answers.js
+++ app/models/answers.js
@@ -1,31 +1,34 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const answerSchema = new Schema({
-    __v: {
-        type: Number,
-        select: false,
+const answerSchema = new Schema(
+    {
+        __v: {
+            type: Number,
+            select: false,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        answerer: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+            select: false,
+        },
+        questionId: {
+            type: String,
+            required: true,
+            select: false,
+        },
+        voteCount: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
     },
-    content: {
-        type: String,
-        required: true,
-    },
-    answerer: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-        select: false,
-    },
-    questionId: {
-        type: String,
-        required: true,
-        select: false,
-    },
-    voteCount: {
-        type: Number,
-        required: true,
-        default: 0,
-    },
-});
+    { timestamps: true }
+);
 
 module.exports = model("Answer", answerSchema);
